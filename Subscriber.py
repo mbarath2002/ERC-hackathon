d2a276c7afd213a505cc4e3200b446301c4bffce
mbarath2002 <mbarath2002@gmail.com>
2021-07-05 12:09:31 +0530

    Update Subscriber.py

--- Subscriber.py
+++ Subscriber.py
@@ -1,1 +1,8 @@
-
+import rospy
+from std_msgs.msg import obstacles
+def callback():
+  print msg.data
+  rospy.init_node('receiver')
+  sub=rospy.Subscriber('path',obstacles,callback)
+  rospy.spin()
+  
