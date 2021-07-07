22305cae653dfb994344e6fb5f71186f539b039a
mbarath2002 <mbarath2002@gmail.com>
2021-07-07 17:22:37 +0200

    try

--- Controller.py
+++
@@ -1,8 +0,0 @@
-import rospy
-from std_msgs.msg import obstacles
-def callback():
-  print msg.data
-  rospy.init_node('receiver')
-  sub=rospy.Subscriber('path',obstacles,callback)
-  rospy.spin()
-  
